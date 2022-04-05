d5e8f00f4ac73a718308b490047808ace54d7392
hit300b1 <hit300b1@gmail.com>
2022-04-05 13:04:46 +0530

    5-apr

--- springprojsirius2022/src/dbpack/DbDemo.java
+++ springprojsirius2022/src/dbpack/DbDemo.java
@@ -1,9 +1,16 @@
 package dbpack;
+//ConfigurableApplicationContext ctx=new ClassPathXmlApplicationContext("config.xml");
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class DbDemo {
 	public static void main(String[] args) throws Exception{
@@ -13,5 +20,31 @@ public class DbDemo {
 		DataSource ds=ctx.getBean("dataSource",DataSource.class);
 		
 		System.out.println(ds.getConnection());
+		
+		JdbcTemplate jdbc=new JdbcTemplate(ds);
+		
+		int noofusers=jdbc.queryForInt("select count(*) from users");
+		
+		System.out.println(noofusers);
+		
+	String name=	jdbc.queryForObject("select uname from users where uid=?", new Object[] {1},String.class);
+		System.out.println(name);
+		
+		User user=jdbc.queryForObject("select * from users where uid=?", new Object[] {1},new MyRowMapper());
+		
+		System.out.println(user);
+	}
+}
+class MyRowMapper implements RowMapper<User>
+{
+	@Override
+	public User mapRow(ResultSet rs, int arg1) throws SQLException {
+		// TODO Auto-generated method stub
+		User user =new User();
+		user.setUid(rs.getInt(1));
+		user.setUname(rs.getString(2));
+		user.setUpass(rs.getString(3));
+		user.setFlag(rs.getInt(4));
+		return user;
 	}
 }
